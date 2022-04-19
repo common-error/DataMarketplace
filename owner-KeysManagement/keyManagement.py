96fd39d21d76cc3ce77a9dfd99ca78c0c61bb80f
common-error <simioni.riccardo96@gmail.com>
2022-04-19 21:40:00 +0200

    bug fix

--- owner-KeysManagement/keyManagement.py
+++ owner-KeysManagement/keyManagement.py
@@ -10,22 +10,32 @@ kds = KDS.KDS()
 
 
 resources = [
-    {"id":"r1","data":b"prima risorsa"},
-    {"id":"r2","data":b"seconda risorsa"},
+    {"id":"a","data":b"risorsa a"},
+    {"id":"b","data":b"risorsa b"},
+    {"id":"c","data":b"risorsa c"},
+    {"id":"d","data":b"risorsa d"},
+    {"id":"e","data":b"risorsa e"},
+    {"id":"f","data":b"risorsa f"},
 ]
 
 
 
 for x in resources:
     kds.addResource(x['id'])
-    print(util.crypt(kds.getResourceEncKey(x['id']),x['data']))
+    #print(util.crypt(kds.getResourceEncKey(x['id']),x['data']))
 
+#kds.show()
+"""
+kds.enforcePurchase("0xD1192bc74BF3b44EEC9ad07271165dD6B6FF8387","w",["a","b","c"])
 kds.show()
-
-kds.enforcePurchase("0xD1192bc74BF3b44EEC9ad07271165dD6B6FF8387",["r1","r2"])
-
+kds.enforcePurchase("0xwdfgsdfc74BF3b44EEC9ad07271165dD6B6FF8387","x",["c","d","e","f"])
 kds.show()
-kds.save()
+kds.enforcePurchase("0xD1192bcsdfgsdfg44EEC9ad07271165dD6B6FF8387","y",["a","b","c"])
+kds.show()
+"""
+#kds.enforcePurchase("0xZcompraBC","z",["b","c"])
+kds.show()
+#kds.save()
 
 
 
