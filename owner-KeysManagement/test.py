467c7769072a9c6eceb64ca0ce9d64910c627858
common-error <simioni.riccardo96@gmail.com>
2022-04-21 10:44:38 +0200

    update

--- owner-KeysManagement/test.py
+++ owner-KeysManagement/test.py
@@ -1,5 +1,5 @@
 from tkinter import Pack
-from brownie import web3
+from brownie import chain, web3
 from cryptography.fernet import Fernet
 import secrets
 import hashlib
@@ -7,6 +7,7 @@ import base64
 import networkx as nx
 import matplotlib.pyplot as pltpip
 from numpy import byte
+from rsa import sign
 
 
 def byte_xor(ba1, ba2):
@@ -160,6 +161,10 @@ print(list(set(c)-set(b)))
 """
 import json
 from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PATH_TO_TRUFFLE = "../smart-contract/build/contracts/"
 trufflefile = json.load(open(PATH_TO_TRUFFLE+"accessAuth.json"))
@@ -168,15 +173,66 @@ abi = trufflefile['abi']
 bytecode = trufflefile['bytecode']
 
 
-ganache_url = "http://127.0.0.1:7545"
+ganache_url = "http://127.0.0.1:8545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
-chain_id = 5777
+chain_id = 1337
 
 print("Connected: {}".format(web3.isConnected()))
 
 
-owner = ("0xA44E86CE0817E7244015B481E59599d56ed26BA9","0xfae34b208c72d5f8590ac8fda25ecc5127fc8c3e26c64b3dac58d8eb04aae399")
-user = "0x484657fCE5228112f83a975AC2c04f8f3D30B4Bd"
 
+owner = {
+    "publicKey" : "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
+    "privateKey" : os.getenv("PRIVATE_KEY")
+}
+user = "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0"
+
+"""
+#Create a contract in python
 accessAuth = web3.eth.contract(abi=abi,bytecode=bytecode)
 print(accessAuth)
+
+#Get the latest transaction
+nonce = web3.eth.getTransactionCount(owner["publicKey"])
+print(nonce)
+
+#Create a transaction
+transaction = accessAuth.constructor().buildTransaction({
+    "gasPrice":web3.eth.gas_price,
+    "chainId":chain_id,
+    "from":owner["publicKey"],
+    "nonce":nonce
+})
+
+signed_txn = web3.eth.account.sign_transaction(transaction,private_key=owner["privateKey"])
+
+tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+
+print(tx_receipt.contractAddress)
+"""
+#working with contracts
+contractAddress = "0x5b1869D9A4C187F2EAa108f3062412ecf0526b24"
+
+accessAuth = web3.eth.contract(address=contractAddress,abi=abi)
+nonce = web3.eth.getTransactionCount(owner["publicKey"])
+print("nonce: {}".format(nonce))
+
+
+
+#Crea una transazione e modifica la blockchain
+strore_transaction = accessAuth.functions.buyResources([1,2]).buildTransaction({
+    "gasPrice":web3.eth.gas_price,
+    "chainId":chain_id,
+    "from":owner["publicKey"],
+    "nonce":nonce
+})
+sign_store_tnx = web3.eth.account.sign_transaction(
+    strore_transaction,
+    private_key=owner["privateKey"]
+)
+send_store_tx = web3.eth.send_raw_transaction(sign_store_tnx.rawTransaction)
+tx_receipt = web3.eth.wait_for_transaction_receipt(send_store_tx)
+
+#Chiama una funzione in sola lettura
+print(accessAuth.functions.getCapabilityListByAddress(owner["publicKey"]).call())
